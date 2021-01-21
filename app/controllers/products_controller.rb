e8e0ff839660601c0aa5853a0a370fee943d0fb3
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-21 07:56:47 -0500

    commit tree 3c42641dc088b56a8e3b86dc0ac29deee26a1248

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+    
+    def index
+    end
+
+    def add_to_cart
+        cart << params[:product]
+        redirect_to products_path
+    end
+
+end
